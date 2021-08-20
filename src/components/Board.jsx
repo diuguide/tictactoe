9d2bc25ce7e4770ac393871ce6fe4c01e574a4ae
diuguide <everett.diuguid@gmail.com>
2021-08-20 02:11:38 -0400

    added use effect logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
 import { boardState, userMove, compMove } from "../features/board/boardSlice";
+import { useEffect } from "react";
 
 const Board = () => {
   const board = useSelector(boardState);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (board.userClicked.length != 0) {
+      computerMove();
+    }
+  }, [board.userClicked]);
 
   const handleClick = (e) => {
     const { id } = e.target;
@@ -12,8 +19,8 @@ const Board = () => {
 
   const userClick = (num) => {
     if (checkClicked(num)) {
-        dispatch(userMove(num));
-        markClicked('green', num);
+      markClicked("green", num);
+      dispatch(userMove(num));
     }
   };
 
@@ -25,10 +32,56 @@ const Board = () => {
   };
 
   const markClicked = (color, num) => {
+    console.log("num inside mark clicked", num);
     let current = document.getElementById(num);
-    current.style.backgroundColor = color;    
-  }
+    current.style.backgroundColor = color;
+  };
 
+  const computerMove = () => {
+    let compChoice = bestMove();
+    if (checkClicked(compChoice)) {
+      console.log("checkClicked computer move");
+      dispatch(compMove(compChoice));
+      markClicked("red", compChoice);
+    }
+  };
+
+  const bestMove = () => {
+    let moveCount = board.userClicked.length;
+    console.log("moveCount: ", moveCount);
+    switch (moveCount) {
+      case 0:
+        for (let i = 0; i < board.possibleWins.length; i++) {
+          if (board.userClicked[0] != board.possibleWins[i][0]) {
+            return board.possibleWins[i][0];
+          }
+        }
+        break;
+      case 1:
+        for (let i = 0; i < board.possibleWins.length; i++) {
+          if (board.userClicked[1] != board.possibleWins[i][1]) {
+            return board.possibleWins[i][1];
+          }
+        }
+        break;
+      case 2:
+        for (let i = 0; i < board.possibleWins.length; i++) {
+          if (board.userClicked[2] != board.possibleWins[i][2]) {
+            return board.possibleWins[i][2];
+          }
+        }
+        break;
+      case 3:
+        for (let i = 0; i < board.possibleWins.length; i++) {
+          if (board.userClicked[0] != board.possibleWins[i][0]) {
+            return board.possibleWins[i][0];
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="fullGrid">
--- src/features/board/boardSlice.js
+++ src/features/board/boardSlice.js
@@ -1,27 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    grid: [0,1,2,3,4,5,6,7,8],
-    userClicked: [],
-    compClicked: [],
-}
+  grid: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  userClicked: [],
+  compClicked: [],
+  possibleWins: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+};
 
 const boardSlice = createSlice({
-    name: "board",
-    initialState,
-    reducers: {
-        userMove: (state, action) => {
-            state.userClicked.push(action.payload);
-        },
-        compMove: (state, action) => {
-            state.compClicked.push(action.payload);
-        },
-        gameOver: (state) => {
-            state.userClicked = [];
-            state.compClicked = [];
-        }
-    }
-})
+  name: "board",
+  initialState,
+  reducers: {
+    userMove: (state, action) => {
+      state.userClicked.push(action.payload);
+    },
+    compMove: (state, action) => {
+      state.compClicked.push(action.payload);
+    },
+    gameOver: (state) => {
+      state.userClicked = [];
+      state.compClicked = [];
+    },
+  },
+});
 
 export const { userMove, compMove, gameOver } = boardSlice.actions;
 export const boardState = (state) => state.board;
